482ae0cc8481a6f18190a00d57a11566dfd09be3
madaxen86 <59310516+madaxen86@users.noreply.github.com>
2024-08-09 21:33:30 +0200

    init 2

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,23 @@
 import { cookieStorage, makePersisted } from "@solid-primitives/storage";
-import { reload } from "@solidjs/router";
 import { createSignal } from "solid-js";
 
 export default function Home() {
   const [text, setText] = makePersisted(createSignal(""), {
     storage: cookieStorage,
     name: "umlauts",
-    serialize(data) {
-      return JSON.stringify(data);
-    },
-    deserialize(data) {
-      return JSON.parse(data);
-    },
   });
 
   return (
     <main>
       <h1>Umlauts</h1>
-
-      <input
-        value={text()}
-        onInput={(e) => setText(e.currentTarget.value)}
-      />
-      <button onClick={() => reload()}>Reload</button>
+      <div class="container">
+        <input
+          value={text()}
+          onInput={(e) => setText(e.currentTarget.value)}
+        />
+        <button onClick={() => setText("öäüÖÄÜ")}>Set umlauts öäüÖÄÜ</button>
+        <button onClick={() => window.location.reload()}>Reload</button>
+      </div>
     </main>
   );
 }
